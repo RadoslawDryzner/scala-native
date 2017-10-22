95a3c8b03bcdda4e6e1fba02af8f6fe0d32afd89
RadoslawDryzner <radoslaw.dryzner@epfl.ch>
2017-10-22 12:53:14 +0200

    Implement native parts of NetworkInterface

--- javalib/src/main/scala/java/net/NetworkInterface.scala
+++ javalib/src/main/scala/java/net/NetworkInterface.scala
@@ -1,7 +1,16 @@
 package java.net
 
 import scala.collection.mutable.MutableList
+import scala.collection.mutable.ArrayBuffer
 import collection.JavaConverters._
+
+import scalanative.native._
+import scalanative.posix.net._if._
+import scalanative.posix.sys.socket._
+import scalanative.posix.sys.ioctl._
+import scalanative.posix.unistd._
+import scalanative.native.ifaddrs._
+import scalanative.posix.netinet.in._
 
 import java.util.Enumeration
 
@@ -94,31 +103,82 @@ final class NetworkInterface private (
     string.toString()
   }
 
-  // TODO : this actually calls only the native static method
-  def getInterfaceAddresses() : List[InterfaceAddress] = ???
+  def getInterfaceAddresses() : java.util.List[InterfaceAddress] = {
+    val ptr = stackalloc[Ptr[ifaddrs]]
+    val ret = ifaddrs.getifaddrs(ptr)
+    val first : Ptr[ifaddrs] = !ptr
+    getInterfaceAddressesImpl(first, List()).asJava
+  }
+
+  private def getInterfaceAddressesImpl(netif : Ptr[ifaddrs.ifaddrs], 
+                                        acc : List[InterfaceAddress]) : List[InterfaceAddress] = {
+    if (netif != null) {
+      val netifName = fromCString(!netif._2)
+      if (netifName == name && !netif._4 != null) {
+        val addr : Ptr[sockaddr] = !netif._4
+        if (!addr._1 == AF_INET.toUInt) {
+          val addr4 = addr.cast[Ptr[sockaddr_in]]
+          val addr4in = !(addr4._3)._1
+          val addrBytes = Array.fill[Byte](4)(0)
+          for (i <- 3 to 0 by -1) {
+            addrBytes(i) = (addr4in.toByte / math.pow(2, i * 8)).toByte
+          }
+          val mask = !netif._5
+          val mask4 = mask.cast[Ptr[sockaddr_in]]
+          val mask4in = !(mask4._3)._1
+          val prefixLength = mask4in.toBinaryString.groupBy(identity).mapValues(_.size)('1')
+          
+          getInterfaceAddressesImpl((!netif._1).cast[Ptr[ifaddrs.ifaddrs]], 
+                                    new InterfaceAddress(
+                                      new Inet4Address(addrBytes), 
+                                      prefixLength.toShort) :: acc)
+        } else if (!addr._1 == AF_INET6.toUInt) {
+          val addr6 = addr.cast[Ptr[Byte]]
+          val addr6in : Ptr[in6_addr] = ((addr6 + 8)).cast[Ptr[in6_addr]]
+          val addrBytes = Array.fill[Byte](16)(0)
+          for (i <- 0 until 16) {
+            addrBytes(i) = (!((addr6in._1)._1 + i)).toByte
+          }
+          val mask = !netif._5
+          val mask6 = mask.cast[Ptr[Byte]]
+          val mask6in = ((mask6 + 8)).cast[Ptr[in6_addr]]
+          val maskBytes = Array.fill[Long](16)(0)
+          for (i <- 0 until 16) {
+            maskBytes(i) = (!((mask6in._1)._1 + i)).toLong
+          }
+          val prefixLength = maskBytes.map(_.toBinaryString.groupBy(identity).mapValues(_.size).getOrElse('1', 0)).reduceLeft(_ + _)
+          
+          getInterfaceAddressesImpl((!netif._1).cast[Ptr[ifaddrs.ifaddrs]], 
+                                    new InterfaceAddress(
+                                      new Inet6Address(addrBytes), 
+                                      prefixLength.toShort) :: acc)
+        } else {
+          getInterfaceAddressesImpl((!netif._1).cast[Ptr[ifaddrs.ifaddrs]], acc)
+        }
+      } else {
+        getInterfaceAddressesImpl((!netif._1).cast[Ptr[ifaddrs.ifaddrs]], acc)
+      }
+    } else {
+      acc
+    }
+  }
 
   def getSubInterfaces() : Enumeration[NetworkInterface] = children.iterator.asJavaEnumeration
 
   def getParent() : NetworkInterface = parent
 
   def isUp() : Boolean = {
-    if (addresses.size == 0) {
+    if (addresses.size == 0)
       false
-    } else {
-      // TODO : call to native method
-      // call to isUpImpl(name, interfaceIndex)
-      // call to getPlatformIsUp(name, index)
-      // call getPlatformNetworkInterfaceAttribute(name, IFF_UP)
-      ???
-    }
+    else
+      getNetworkInterfaceAttribute(IFF_RUNNING)
   }
 
   def isLoopback() : Boolean = {
     if (addresses.size == 0) {
       false
     } else {
-      // TODO : call to native method
-      ???
+      getNetworkInterfaceAttribute(IFF_LOOPBACK)
     }
   }
 
@@ -126,8 +186,7 @@ final class NetworkInterface private (
     if (addresses.size == 0) {
       false
     } else {
-      // TODO : call to native method
-      ???
+      getNetworkInterfaceAttribute(IFF_POINTOPOINT)
     }
   }
 
@@ -135,8 +194,7 @@ final class NetworkInterface private (
     if (addresses.size == 0) {
       false
     } else {
-      // TODO : call to native method
-      ???
+      getNetworkInterfaceAttribute(IFF_MULTICAST)
     }
   }
 
@@ -144,8 +202,42 @@ final class NetworkInterface private (
     if (addresses.size == 0) {
       Array[Byte]()
     } else {
-      // TODO : call to native method
-      ???
+      val ifreqSize = 40
+      val ifnameSize = 16
+      val macAddrSize = 6
+      val toReturn = Array.fill[Byte](macAddrSize)(0)
+      val fd = socket(AF_INET, SOCK_STREAM, 0)
+      if (fd < 0) {
+        throw new SocketException("The operation failed with no recovery possible")
+      }
+      val ifc = stackalloc[ifconf]
+      val buf = stackalloc[Byte](2048)
+      !ifc._1 = 2048
+      !ifc._2 = buf
+      if (ioctl(fd, SIOCGIFCONF, ifc.cast[Ptr[Byte]]) != 0){
+        throw new SocketException("The operation failed with no recovery possible")
+      }
+
+      val ifr = !ifc._2
+      val n = !ifc._1 / ifreqSize
+
+      for (i <- 0 until n.toInt) {
+        val ifname = (ifr + (i * ifreqSize))
+        if (fromCString(ifname) == name) {
+          if(ioctl(fd, SIOCGIFHWADDR, (ifr + (i * ifreqSize))) != 0){
+            close(fd)
+            throw new SocketException("Could not retrieve information about socket.")
+          }
+          // get array
+          for(j <- 0 until macAddrSize) {
+            toReturn(j) = !((((ifname + ifnameSize).cast[Ptr[sockaddr]])._2)._1 + j)
+          }
+          close(fd)
+          return toReturn
+        }
+      }
+      close(fd)
+      Array[Byte]()
     }
   }
 
@@ -153,17 +245,130 @@ final class NetworkInterface private (
     if (addresses.size == 0) {
       0
     } else {
-      // TODO : call to native method
-      ???
+      val ifreqSize = 40
+      val ifnameSize = 16
+      val fd = socket(AF_INET, SOCK_STREAM, 0)
+      if (fd < 0) {
+        throw new SocketException("The operation failed with no recovery possible")
+      }
+      val ifc = stackalloc[ifconf]
+      val buf = stackalloc[Byte](2048)
+      !ifc._1 = 2048
+      !ifc._2 = buf
+      if (ioctl(fd, SIOCGIFCONF, ifc.cast[Ptr[Byte]]) != 0){
+        throw new SocketException("The operation failed with no recovery possible")
+      }
+
+      val ifr = !ifc._2
+      val n = !ifc._1 / ifreqSize
+
+      for (i <- 0 until n.toInt) {
+        val ifname = (ifr + (i * ifreqSize))
+        if (fromCString(ifname) == name) {
+          if(ioctl(fd, SIOCGIFMTU, (ifr + (i * ifreqSize))) != 0){
+            close(fd)
+            throw new SocketException("Could not retrieve information about socket.")
+          }
+          val mtu = !((ifname + ifnameSize).cast[Ptr[CInt]])
+          close(fd)
+          return mtu
+        }
+      }
+      close(fd)
+      0
     }
   }
 
   def isVirtual() : Boolean = parent != null
+
+  private def getNetworkInterfaceAttribute(iiFlag : Long) : Boolean = {
+    val ifreqSize = 40
+    val ifnameSize = 16
+    val fd = socket(AF_INET, SOCK_STREAM, 0)
+    if (fd < 0) {
+      throw new SocketException("The operation failed with no recovery possible")
+    }
+    val ifc = stackalloc[ifconf]
+    val buf = stackalloc[Byte](2048)
+    !ifc._1 = 2048
+    !ifc._2 = buf
+    if (ioctl(fd, SIOCGIFCONF, ifc.cast[Ptr[Byte]]) != 0){
+      throw new SocketException("The operation failed with no recovery possible")
+    }
+
+    val ifr = !ifc._2
+    val n = !ifc._1 / ifreqSize
+
+    for (i <- 0 until n.toInt) {
+      val ifname = (ifr + (i * ifreqSize))
+      if (fromCString(ifname) == name) {
+        if(ioctl(fd, SIOCGIFFLAGS, (ifr + (i * ifreqSize))) != 0){
+          close(fd)
+          throw new SocketException("Could not retrieve information about socket.")
+        }
+        val flags = !((ifname + ifnameSize).cast[Ptr[CShort]])
+        close(fd)
+        return (flags & iiFlag) == iiFlag
+      }
+    }
+    close(fd)
+    false
+  }
 }
 
 object NetworkInterface {
-  // TODO : native
-  private def getNetworkInterfacesImpl() : Array[NetworkInterface] = ???
+  private def getNetworkInterfacesImpl() : Array[NetworkInterface] = {
+    val ptr = stackalloc[Ptr[ifaddrs]]
+    val ret = ifaddrs.getifaddrs(ptr)
+    val first : Ptr[ifaddrs] = !ptr
+    traverseInterfaces(first, ArrayBuffer.empty[NetworkInterface])
+  }
+
+  private def traverseInterfaces(netif : Ptr[ifaddrs], acc : ArrayBuffer[NetworkInterface]) : Array[NetworkInterface] = {
+    if (netif != null) {
+      val netifName = fromCString(!netif._2)
+      val currNetif = acc.find(_.getName() == netifName) match {
+        case Some(someNetif) => someNetif
+        case None => new NetworkInterface(netifName, netifName, if_nametoindex(!netif._2).toInt)
+      }
+      if (!netif._4 != null) {
+        val addr : Ptr[sockaddr] = !netif._4
+        if (!addr._1 == AF_INET.toUInt) {
+          val addr4 = addr.cast[Ptr[sockaddr_in]]
+          val addr4in = !(addr4._3)._1
+          val addrBytes = Array.fill[Byte](4)(0)
+          for (i <- 3 to 0 by -1) {
+            addrBytes(i) = (addr4in.toByte / math.pow(2, i * 8)).toByte
+          }
+          val inet = new Inet4Address(addrBytes)
+          currNetif.addresses += inet
+          acc.find(_.getName() == netifName) match {
+            case Some(someNetif) => traverseInterfaces((!netif._1).cast[Ptr[ifaddrs]], acc)
+            case None => traverseInterfaces((!netif._1).cast[Ptr[ifaddrs]], acc += currNetif)
+          }
+        } else if (!addr._1 == AF_INET6.toUInt) {
+          val addr6 = addr.cast[Ptr[Byte]]
+          val addr6in : Ptr[in6_addr] = ((addr6 + 8)).cast[Ptr[in6_addr]]
+          val addrBytes = Array.fill[Byte](16)(0)
+          for (i <- 0 until 16) {
+            addrBytes(i) = (!((addr6in._1)._1 + i)).toByte
+          }
+          val inet = new Inet6Address(addrBytes)
+          currNetif.addresses += inet
+          acc.find(_.getName() == netifName) match {
+            case Some(someNetif) => traverseInterfaces((!netif._1).cast[Ptr[ifaddrs]], acc)
+            case None => traverseInterfaces((!netif._1).cast[Ptr[ifaddrs]], acc += currNetif)
+          }
+        } else {
+          traverseInterfaces((!netif._1).cast[Ptr[ifaddrs]], acc)
+        }
+      } else {
+        traverseInterfaces((!netif._1).cast[Ptr[ifaddrs]], acc += currNetif)
+      }
+    } else {
+      acc.toArray
+    }
+  }
 
   def getByName(interfaceName : String) : NetworkInterface = {
     if (interfaceName == null) {
@@ -202,7 +407,6 @@ object NetworkInterface {
     } else {
       val peeked = Array.fill[Boolean](interfaces.size)(false)
 
-      // TODO : check if these fields are good
       interfaces.foreach(netif => netif.addresses.foreach(addr => {
         if (addr.isLinkLocalAddress() || addr.isSiteLocalAddress()) {
           addr.asInstanceOf[Inet6Address].scopedIf = netif
