f925acda9bde812a51a07cbb527ffe1d7705ffce
RadoslawDryzner <radoslaw.dryzner@epfl.ch>
2017-11-12 00:50:02 +0100

    Make fields access by name in ifconf

--- nativelib/src/main/scala/scala/scalanative/posix/net/if.scala
+++ nativelib/src/main/scala/scala/scalanative/posix/net/if.scala
@@ -7,7 +7,7 @@ import scalanative.native._
 @extern
 object _if {
   type ifconf = CStruct2[CInt,
-                         Ptr[Byte]]
+                         CString]
 
   def if_nametoindex(ifname : CString) : UInt = extern
 
@@ -22,4 +22,16 @@ object _if {
 
   @name("scalanative_IFF_MULTICAST")
   def IFF_MULTICAST : CInt = extern
+}
+
+object ifOps {
+  import _if._
+
+  implicit class ifOps(val ptr: Ptr[ifconf]) extends AnyVal {
+    def if_index: CInt = !ptr._1
+    def if_name: CString = !ptr._2
+
+    def if_index_=(v: CInt) = !ptr._1 = v
+    def if_name_=(v: CString) = !ptr._2 = v
+  }
 }
